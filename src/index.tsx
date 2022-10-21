c5b6f023ecf7924101d0dca892840163941d4e73
Abe Yoshida <abeyoshida@gmail.com>
2022-10-21 17:10:49 -0400

    CRA updates

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>YO</h1>
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
